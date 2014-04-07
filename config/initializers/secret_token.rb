d893da62a298d33685914f1d61a686cb5942b4f3
Fiona Bianchi <fiona@katieandfiona.co.uk>
2014-04-07 18:26:34 +0100

    Adding basic files from new app creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CompetencyTestingQuestions::Application.config.secret_token = '2754386204aeadc2145cd2b06e948100b0007e66b120bd17c83161ac74405f49edbf810210f91d22b84d5abab65d0e1affd9806eb4f5566adbe4d03800353b05'
